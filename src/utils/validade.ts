3e44c657bcc75fa4805484d44261302af5fcd028
Vitor <vitorhaoliveira123@gmail.com>
2024-05-17 11:19:28 -0300

    send to deploy

---
+++ src/utils/validade.ts
@@ -0,0 +1,23 @@
+import { User } from "../types/User";
+
+type Error = {
+   [key: string]: string;
+}
+
+export const validate = (data: User) => {
+   const error: Error = {};
+
+   if (!data.name) {
+      error['name'] = 'O nome é obrigatório!'
+   }
+
+   if (!data.email) {
+      error['email'] = 'O email é obrigatório!'
+   }
+
+   if (!data.agree) {
+      error['agree'] = 'Você precisa concordar com os termos!'
+   }
+
+   return error;
+}
